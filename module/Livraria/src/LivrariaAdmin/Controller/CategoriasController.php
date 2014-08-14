d6b669ebc6580b8b19850e85cb1bc595fe2e44f3
dil <dil@dil-linux>
2014-08-14 15:58:04 -0300

    Onde parei: Básico parte 7
    Abstrai os Services e o CRUD, assim na próxima vez que eu for criar eles vai ficar mais facil

--- module/Livraria/src/LivrariaAdmin/Controller/CategoriasController.php
+++ module/Livraria/src/LivrariaAdmin/Controller/CategoriasController.php
@@ -2,87 +2,14 @@
 
 namespace LivrariaAdmin\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController,
-    Zend\View\Model\ViewModel;
+class CategoriasController extends CRUDController {
 
-use Zend\Paginator\Paginator,
-    Zend\Paginator\Adapter\ArrayAdapter;
-
-use LivrariaAdmin\Form\Categoria as FrmCategoria;
-
-class CategoriasController extends AbstractActionController {
-
-    protected $em;
-
-    public function indexAction() {
-        $list = $this->getEm()->getRepository('Livraria\Entity\Categoria')->findAll();
-
-        $page = $this->params()->fromRoute('page');
-
-        $paginator = new Paginator(new ArrayAdapter($list));
-        $paginator->setCurrentPageNumber($page);
-        $paginator->setDefaultItemCountPerPage(4);
-
-        return new ViewModel(array('data' => $paginator, 'page' => $page));
+    public function __construct() {
+        $this->entity = "Livraria\Entity\Categoria";
+        $this->form = "LivrariaAdmin\Form\Categoria";
+        $this->service = "Livraria\Service\Categoria";
+        $this->controller = "categorias";
+        $this->route = "livraria-admin";
     }
-
-    public function newAction() {
-        $form = new FrmCategoria();
-        $request = $this->getRequest();
-
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-            if ($form->isValid()) {
-                $service = $this->getServiceLocator()->get('Livraria\Service\Categoria');
-                $service->insert($request->getPost()->toArray());
-
-                return $this->redirect()->toRoute('livraria-admin', array('controller' => 'categorias'));
-            }
-        }
-
-        return new ViewModel(array('form' => $form));
-    }
-
-    public function editAction() {
-        $form = new FrmCategoria();
-        $request = $this->getRequest();
-
-        $repository = $this->getEm()->getRepository('Livraria\Entity\Categoria');
-        $entity = $repository->find($this->params()->fromRoute('id', 0));
-
-        if ($this->params()->fromRoute('id', 0)) 
-            $form->setData($entity->toArray());
-            
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-            if ($form->isValid()) {
-                $service = $this->getServiceLocator()->get('Livraria\Service\Categoria');
-                $service->update($request->getPost()->toArray());
-
-                return $this->redirect()->toRoute('livraria-admin', array('controller' => 'categorias'));
-            }
-        }
-          
-        return new ViewModel(array('form' => $form));
-    }
-
-    public function deleteAction() {
-        $service = $this->getServiceLocator()->get('Livraria\Service\Categoria');
-        
-        if ($service->delete($this->params()->fromRoute('id',0))) {
-            return $this->redirect()->toRoute('livraria-admin', array('controller'=>'categorias'));
-        }   
-    }    
     
-    /**
-     * @return EntityManager
-     */
-    protected function getEm() {
-        if ($this->em == null) {
-            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-        }
-
-        return $this->em;
-    }
-
 }
